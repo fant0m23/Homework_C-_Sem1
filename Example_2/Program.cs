2bdb225f7c1706381a3464077d9fe4d80d1368d6
Andrey_Kiselev <fant0m2311@gmail.com>
2023-02-17 13:11:23 +0300

    Ex_2 decided

--- Example_2/Program.cs
+++ Example_2/Program.cs
@@ -4,5 +4,17 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-
-Console.WriteLine();
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+   Console.Write("Ответ: max = ");
+   Console.Write(a);
+}
+else
+{
+   Console.Write("Ответ: max = ");
+   Console.Write(b);
+}
